ca0a5ced5f16c378df9ddea55c6c6f3c842b9175
Willem Goudsbloem <willem.goudsbloem@gmail.com>
2014-06-13 16:02:35 -0400

    type is returning only distinct

--- api/search/product/type.js
+++ api/search/product/type.js
@@ -2,28 +2,25 @@ var mongodb = require("mongodb");
 var Q = require("q");
 
 module.exports = function(q) {
-	var deferred = Q.defer();
-	mongodb.MongoClient.connect("mongodb://test:test123@troup.mongohq.com:10049/ratelus", function(err, db) {
-		if (err !== null) {
-			console.error(err);
-		}
-		db.collection('products', function(err, collection) {
-			if (err !== null) {
-				console.error(err);
-			}
-			var result = collection.find({
-				$or : [ {
-					"type" : new RegExp("^" + q, "i")
-				} ]
-			}, { _id: 0 });
-
-			result.toArray(function(err, items) {
-				deferred.resolve(items);
-				if (err !== undefined) {
-					deferred.reject(err);
-				}
-			});
-		});
-	});
-	return deferred.promise;
+  var deferred = Q.defer();
+  mongodb.MongoClient.connect("mongodb://test:test123@troup.mongohq.com:10049/ratelus", function(err, db) {
+    if (err !== null) {
+      console.error(err);
+    }
+    db.collection('products', function(err, products) {
+      products.distinct('type'
+          , {
+            $or: [{
+                "type": new RegExp("^" + q, "i")
+              }]
+          }
+      , function(err, items) {
+        deferred.resolve(items);
+        if (err !== undefined) {
+          deferred.reject(err);
+        }
+      });
+    });
+  });
+  return deferred.promise;
 };
